718bd0dc54073b18efc10b3007f7fc7d227aabf4
Ionela Alexandra <sauionela51@gmail.com>
2023-12-16 20:16:39 +0200

    added main module

--- Proiect - Password manager/pwmanager.py
+++ Proiect - Password manager/pwmanager.py
@@ -1,3 +1,5 @@
+import sqlite3
+import sys
 import cryptocode
 
 parola_master = 'ionela2002'
@@ -21,3 +23,64 @@ def criptare_parola(parola):
 def decriptare_parola(parola_criptata):
     parola_decriptata = cryptocode.decrypt(parola_criptata, parola_master)
     return parola_decriptata
+
+def adaugare_parola(website, username, parola, cursor):
+
+
+
+def obtinere_parola(website, cursor):
+
+
+
+def stergere_parola(website, cursor):
+
+
+
+def listare_parole(cursor):
+
+
+
+def conectare_la_baza_de_date():
+
+
+
+def main():
+    parola_introdusa_de_utilizator = sys.argv[1]
+    operatie = sys.argv[2]
+
+    if verificare_parola(parola_introdusa_de_utilizator) == False:
+        return False
+
+    conn, cursor = conectare_la_baza_de_date()
+
+    # Executare operații în funcție de argumentele primite
+    if operatie == '-add':
+        website, username, parola = sys.argv[3:]
+        adaugare_parola(website, username, parola, cursor)
+        conn.commit()
+        conn.close()
+
+    elif operatie == '-get':
+        website = sys.argv[3]
+        obtinere_parola(website, cursor)
+        conn.commit()
+        conn.close()
+
+    elif operatie == '-remove':
+        website = sys.argv[3]
+        stergere_parola(website, cursor)
+        conn.commit()
+        conn.close()
+
+    elif operatie == '-list':
+        listare_parole(cursor)
+        conn.commit()
+        conn.close()
+
+    else:
+        print("Comanda invalida. Vezi instructiunile de utilizare.")
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
